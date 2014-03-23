274aa4c526cd05ade2b42702959cd0cfb7b9df8c
Jacob Abernethy <jaber@seas.upenn.edu>
2014-03-23 18:17:44 -0400

    it's alive...

--- 2048.py
+++ 2048.py
@@ -3,6 +3,7 @@ import numpy as np
 from time import sleep
 
 
+
 def shift(arr):
 	nonzeros = sum(arr>0)
 	arr[:nonzeros] = arr[arr > 0]
@@ -10,49 +11,55 @@ def shift(arr):
 
 def arrange(arr):
 	shift(arr)
-	indold, indnew = 0,0
-	updateflag = False
-	while True:
-		if indnew >= len(arr): break
-		if indold < len(arr) - 1:
-			if arr[indold] == arr[indold+1]:
-				arr[indnew] = arr[indold]*2
-				indold = indold + 2
-			else:
-				arr[indnew] = arr[indold]
-				indold += 1
-		else: arr[indnew] = 0
-		indnew += 1
-
-# rrr = np.array([0, 2, 4,4 ,2, 0, 2, 2, 4, 32, 32, 32, 0, 0, 4, 4, 4])
-
-# arrange(rrr)
-# print rrr
+	for ind in range(len(arr) - 1):
+		if arr[ind] == arr[ind+1]:
+			arr[ind],arr[ind+1] = 2 * arr[ind], 0
+	shift(arr)
 
 
 class TileGame(object):
-	def __init__(self):
+	def __init__(self,X = None):
 		super(TileGame, self).__init__()
+		# if X is None:
 		self.X = np.array([
 					[0,0,0,0],
 					[0,0,0,0],
 					[0,0,0,0],
 					[0,0,0,0]])
-		for numiter in range(2): self.add_random_piece()
+		if X is not None:
+			self.X = X.copy()
+			# for i in range(4):
+			# 	for j in range(4):
+			# 		self.X[i,j] = X[i,j]
+		else:
+			for numiter in range(2):
+				if not self.add_random_piece():
+					print "this is happenng at the beginning!"
+		 # self.X = X.copy()
+		self.num_moves = 0
+
+	def score(self):
+		return self.X.sum()
 
 	def add_random_piece(self):
 		pair = self.pick_rand_slot()
 		if pair is False:
 			print "Game is finished! You LOSE!"
+			# print self.X
 			return False
 		else:
 			i,j = pair
 			self.X[i,j] = 2 if np.random.rand() < .9 else 4
+			return True
 
 	def get_open_slots(self):
-		return [(i,j) for i in range(4) for j in range(4) if self.X[i,j] == 0] 
+		return [(i,j) for i in range(4) for j in range(4) if self.X[i,j] == 0]
+
+	def get_full_slots(self):
+	 	return [(i,j) for i in range(4) for j in range(4) if self.X[i,j] != 0] 
 
 	def pick_rand_slot(self):
+		# print "open slots: " + str(self.get_open_slots())
 		open_slots = self.get_open_slots()
 		if len(open_slots) == 0:
 			return False
@@ -63,37 +70,89 @@ class TileGame(object):
 		return "\n".join(map( 
 					lambda row: "\t".join(map(
 						lambda v: str(v) if v > 0 else "", 
-						row)),
-					self.X))
+						row)), self.X))
+
+	def is_dead(self):
+		if len(self.get_open_slots()) > 0: return False
+		for i,j in self.get_full_slots():
+			for pair in [(i,j+1), (i+1,j)]:
+				(newi,newj) = pair
+				if newi > 3 or newj > 3: continue
+				if self.X[i,j] == self.X[newi,newj]: return False
+		return True
 
 	def move(self,action=None):
 		if action is None: action = np.random.choice(['up','down','left','right'])
-		print "**** You are moving %s ****" % action
+		oldX = self.X.copy()
 		for index in range(4):
 			if action == 'up': lst = self.X[:,index]
 			if action == 'down': lst = self.X[::-1,index]
 			if action == 'left': lst = self.X[index,:]
 			if action == 'right': lst = self.X[index,::-1]
 			arrange(lst)
-		self.add_random_piece()
+		notchanged = (oldX == self.X).all()
+		if notchanged: return False
+		else:
+			if not self.add_random_piece():
+				print "what is this"
+				print oldX
+				print self.X
+				print "hello there"
+			self.num_moves += 1
+		return True
+
+	def run_until_dead(self):
+		while not self.is_dead(): self.move()
+
+	def estimate_score(self,numiter=50):
+		vals = []
+		for i in range(numiter):
+			# print "ITER %i" % i
+			t = TileGame(self.X)
+			t.run_until_dead()
+			vals.append(t.score())
+		return np.mean(vals)
+
+	def estimate_moves(self):
+		perfs = dict()
+		for action in ['up','down','left','right']:
+			# print "checking action %s" % action
+			t = TileGame(self.X)
+			response = t.move(action=action)
+			if response:
+				perfs[action] = t.estimate_score()
+		return perfs
+
+	def take_best_move(self):
+		perfs = self.estimate_moves()
+		best_action = max(perfs.keys(), key=lambda x: perfs[x])
+		self.move(best_action)
+		return best_action
 
 
 
-	# def arrange_row(self,rowind,rev=False):
-	# 	indices = [(rowind,i) for in range(4)]
-	# 	if rev: indices.reverse()
-	# 	tiles = [self.X[i][j] for (i,j) in indices if self.X[i][j] is not None]
-	# 	new_tiles = arrange(tiles)
-	# 	for pair_ind, (i,j) in enumerate(indices)
 
 if __name__ == '__main__':
 	t = TileGame()
 	print t
-	for i in range(10):
-		print "*" * 25
-		t.move()
+	for i in range(4):
+	# while not t.is_dead():
+		print "best action is %s" % t.take_best_move()
 		print t
-		sleep(0.3)
+	# numiter = 0
+	# while not t.is_dead():
+	# 	numiter += 1
+	# 	# print "*" * 25
+	# 	# print "BEFORE--------------------"
+	# 	# print t
+	# 	t.move()
+	# 	# print "AFTER---------------------"
+	# 	# print t 
+	# 	# sleep(0.3)
+	# print t
+	# print numiter
+	# print t.X.sum()
+
 
 	
 
